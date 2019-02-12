7b2f45390a8e0503a5697fcada55167fadd10871
larahurtadojose <2774283+larahurtadojose@users.noreply.github.com>
2019-02-12 10:30:33 -0600

    Update uploadSftp.sh

--- files/uploadSftp.sh
+++ files/uploadSftp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# This script require sshpass and sftp installed and configured first
+# Este script requiere sshpass sftp y ncat instalado y configurado primero
 #
 # Author: Juan Lara
 #
@@ -9,9 +9,11 @@ usr="$1"
 pass="$2"
 maquina="$3"
 componente="$6"
+proxy="$7"
+puerto="$8"
 
 export SSHPASS=$pass
-sshpass -e sftp -oBatchMode=no -b - $usr@$maquina << !
+sshpass -e sftp -oBatchMode=no -o ConnectTimeout=3 -o ProxyCommand='/usr/bin/ncat --proxy-type http --proxy "$7":"$8" %h %p' -oPort=22 -b - $usr@$maquina << !
 cd $4
 put $componente
 bye
